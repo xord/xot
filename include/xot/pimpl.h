467c963ecd50721860f558be84672b5ad773460d
xordog <xordog@gmail.com>
2023-05-17 15:17:36 +0900

    fix crash on copying to null pimpl

--- include/xot/pimpl.h
+++ include/xot/pimpl.h
@@ -34,7 +34,13 @@ namespace Xot
 			{
 				if (&obj == this) return *this;
 
-				*this->get() = *obj.get();
+				if (!obj)
+					this->reset();
+				else if (!this->get())
+					this->reset(new T(*obj));
+				else
+					*this->get() = *obj.get();
+
 				return *this;
 			}
 
