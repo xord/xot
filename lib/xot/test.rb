3378dbc41a58fe5953a6468b44b38fcb45bf10af
xordog <xordog@gmail.com>
2025-04-28 03:22:22 +0900

    Add more tests for processors

--- lib/xot/test.rb
+++ lib/xot/test.rb
@@ -17,8 +17,16 @@ module Xot
     end
 
     def assert_each_in_epsilon(expected, actual, *args)
+      assert_equal expected.size, actual.size
       expected.zip(actual) do |e, a|
         assert_in_epsilon e, a, *args
+      end
+    end
+
+    def assert_each_in_delta(expected, actual, *args)
+      assert_equal expected.size, actual.size
+      expected.zip(actual) do |e, a|
+        assert_in_delta e, a, *args
       end
     end
 
